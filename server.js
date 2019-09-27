dc261d9f5e853fca7ec7d4213028db50587489d1
Scott Roberts <sroberts.kia@gmail.com>
2019-09-27 20:46:28 +0000

    initialized back-end repo

---
+++ server.js
@@ -0,0 +1,26 @@
+const Express = require('express');
+const App = Express();
+const BodyParser = require('body-parser');
+const PORT = 8080;
+require('dotenv').config();
+
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+
+// Express Configuration
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(Express.static('public'));
+
+// Sample GET route
+App.get('/api/data', (req, res) => res.json({
+  message: "Seems to work!",
+}));
+
+App.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+});
