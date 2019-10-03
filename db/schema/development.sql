9e4b5bd469921d4fb494ec429b29deed8ec29976
Amanda Shalansky <amandashalansky@Amandas-MacBook-Pro.local>
2019-10-03 16:24:18 -0600

    added register route

--- db/schema/development.sql
+++ db/schema/development.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (username, password, city) VALUES ('my_email@gmail.com', 'password', 'Calgary');
-INSERT INTO users (username, password, city) VALUES ('your_email@email.com', 'password', 'Edmonton');
+INSERT INTO users (email, username, password, city) VALUES ('my_email@gmail.com', 'myemail', 'password', 'Calgary');
+INSERT INTO users (email, username, password, city) VALUES ('your_email@email.com', 'youremail', 'password', 'Edmonton');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
 
 module.exports = db => {
-  router.get("/", (req,res) => res.json({
-    message: "in users",
-  }));
-  
+  router.get("/", (req,res) => {
+    db.query(`SELECT * FROM users;`).then(({rows: users}) => {
+      res.json(
+        users
+      )
+    })
+  });
+  router.post("/register", (req, res) => {
+    const { username, city, password, email } = req.body;
+    db.query(`INSERT INTO users (username, city, password, email)
+    VALUES ($1, $2, $3, $4)`, [username, city, password, email])
+    .then(() => "hello");
+  });
+
+  router.post("/login", (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+    
+    db.query(`SELECT * FROM users 
+    WHERE username = $1`, [username])
+  })
   return router;
 };
